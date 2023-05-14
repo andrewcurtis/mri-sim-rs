efabf215f2169bfb802f4a4b382182a443f3bc7a
Andrew Curtis <andrew.curtis@gmail.com>
2023-05-14 16:12:33 -0400

    Add epg-py crate

---
+++ crates/epg-py/src/lib.rs
@@ -0,0 +1,48 @@
+//! lib.rs
+#[macro_use]
+extern crate pyo3_built;
+extern crate pyo3;
+
+extern crate epg;
+
+use pyo3::prelude::*; 
+
+use std::f64::consts::PI;
+
+
+#[pyfunction]
+fn demo() {
+    println!("Hello, world!");
+
+    let params = epg::sequences::space::SpaceParams {
+        etl: 220,
+        esp: 0.01,
+        t1: 0.58,
+        t2: 0.11,
+        refocus_angle: PI,
+        cpmg_phase: PI / 2.0,
+        debug_print: false,
+    };
+    let res = epg::sequences::space::simulate(params);
+    //println!("{:?}", res);
+
+}
+
+
+#[allow(dead_code)]
+mod build {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn epg_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(demo, m)?)?;
+
+    m.add("__build__", pyo3_built!(_py, build))?;
+    Ok(())
+} 
+
