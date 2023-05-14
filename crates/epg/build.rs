163409dbf20bcd589f7577278dd6d2e96e7390e3
Andrew Curtis <andrew.curtis@gmail.com>
2023-05-14 16:10:50 -0400

    Cargo check passing on workspace

--- crates/epg/build.rs
+++ crates/epg/build.rs
@@ -3,6 +3,7 @@ extern crate built;
 
 fn main() {
     let src = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let src_dir = std::path::Path::new(&src).join("../..");
     let dst = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("built.rs");
     let mut opts = built::Options::default();
 
@@ -10,7 +11,7 @@ fn main() {
         .set_compiler(true)
         .set_env(true);
 
-    built::write_built_file_with_opts(&opts, std::path::Path::new(&src), &dst)
+    built::write_built_file_with_opts(&opts, &src_dir, &dst)
         .expect("Failed to acquire build-time information");
 }
 
--- crates/epg/src/lib.rs
+++ crates/epg/src/lib.rs
@@ -1,12 +1,9 @@
 //! lib.rs
 
-
-use std::f64::consts::PI;
-
-mod epg;
+pub mod epg;
 pub mod sequences;
 pub mod types;
-//mod tissues;
+pub mod tissues;
 
 
 
